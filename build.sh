21d3c1c3e6b80c93442015ec26c3b9f784488f76
Thiago Antunes <53614138+smoothzz@users.noreply.github.com>
2024-12-29 23:19:36 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,20 +4,6 @@ set -ouex pipefail
 
 RELEASE="$(rpm -E %fedora)"
 
+rpm-ostree install gnome-boxes gnome-software python3-pip timeshift touchegg NetworkManager-adsl NetworkManager-bluetooth NetworkManager-iodine-gnome NetworkManager-l2tp-gnome NetworkManager-libreswan-gnome NetworkManager-openconnect-gnome NetworkManager-openvpn-gnome NetworkManager-ppp NetworkManager-pptp-gnome NetworkManager-vpnc-gnome NetworkManager-wifi NetworkManager-wwan abrt-desktop abrt-java-connector blueman cinnamon cinnamon-control-center cinnamon-screensaver eom ffmpegthumbnailer firewall-config gnome-disk-utility gnome-screenshot gnome-system-monitor gnome-terminal gstreamer1-plugins-ugly-free gvfs-archive gvfs-gphoto2 gvfs-mtp gvfs-smb imsettings-gsettings initial-setup-gui metacity nemo-fileroller nemo-image-converter nemo-preview nemo-python nm-connection-editor paper-icon-theme pipewire-alsa pipewire-pulseaudio powerline qgnomeplatform-qt5 redshift-gtk sane-backends-drivers-scanners setroubleshoot simple-scan slick-greeter slick-greeter-cinnamon system-config-printer vim-powerline xawtv xdg-user-dirs-gtk xed xfburn xreader  libvirtd  tilix flameshot ulauncher zsh numix-icon-theme-circle cockpit cockpit-machines podman git ffmpeg-libs virt-install libvirt-daemon-config-network libvirt-daemon-kvm qemu-kvm virt-viewer vscode
 
-### Install packages
-
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
-
-# this installs a package from fedora repos
-rpm-ostree install screen
-
-# this would install a package from rpmfusion
-# rpm-ostree install vlc
-
-#### Example for enabling a System Unit File
-
-systemctl enable podman.socket
+systemctl enable podman.socket cockpit.socket libvirtd
